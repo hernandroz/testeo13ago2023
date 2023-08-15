7660849ab7e1472f077acfadfd865029a9e8777b
Hernán Hernández Rodríguez <83633647+hernandroz@users.noreply.github.com>
2023-08-15 18:25:38 -0500

    Actualizando main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = FastAPI()
 df_general = pd.read_parquet('asdf.parquet')
 df_business = pd.read_parquet('business_google_yelp.parquet')
 
-
 def get_business_user(user_id):
     # Se ingresa un 'User_ID' y retorna un DataFrame con los negocios que visitó que tengan mayor o igual a 4 estrellas
 
@@ -17,17 +16,24 @@ def get_business_user(user_id):
 
     return set(e['ID_Business'])
 
+
 def get_top_business_state(state):
-    top = df_general[df_general['State'] == state]
-    top = top.groupby('ID_Business', as_index = False)['Stars'].mean()
-    top = set(top[top['Stars']==5]['ID_Business'])
+    # Filtramos el dataset por estado:
+    top = df_business[df_business['State'] == state]
+    # Para evitar un posible sesgo, solo tomaremos negocios que tengan un mínimo de 15 reseñas:
+    top = top[top['Review_count']>=15]
+    # Por último, nos quedaremos con aquellos negocios que tengan un mínimo de 4.5 estrellas de promedio:
+    top = set(top[top['Stars']>=4.5]['ID_Business'])
     
     return top 
+
 
 def get_states(User_ID):
     return list(df_general[df_general['User_ID'] == User_ID]['State'].unique())
 
+
 def nombre_negocio(lista_ids):
+    # Convierte una lista de id de negocios en una lista con los nombres respectivos de los negocios
     df_business = pd.read_parquet('business_google_yelp.parquet')
     recomendacion_texto2 = []
     for id in lista_ids:
@@ -38,7 +44,7 @@ def nombre_negocio(lista_ids):
 
 
 @app.get("/recomendacion/{user_id}")
-def recomendacion(user_id):
+def recomendacion2(user_id):
     states = get_states(user_id)
     recomendacion_stateS = []
     for state in states:
